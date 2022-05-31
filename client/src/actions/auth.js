876df94afc897f8f136911a8ef4a7bd9e34e892a
Aadil Saudagar <aadilsaudagar244@gmail.com>
2022-05-31 10:37:35 +0530

    final

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,75 +1,75 @@
-import { BASE_URL } from '../utils';
-import api from '../utils/api';
-import { setAlert } from './alert';
+import { BASE_URL } from "../utils";
+import api from "../utils/api";
+import { setAlert } from "./alert";
 import {
-	REGISTER_SUCCESS,
-	REGISTER_FAIL,
-	USER_LOADED,
-	AUTH_ERROR,
-	LOGIN_SUCCESS,
-	LOGIN_FAIL,
-	LOGOUT,
-} from './types';
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+} from "./types";
 
 export const loadUser = () => async (dispatch) => {
-	try {
-		const res = await api.get(`${BASE_URL}/auth`);
+  try {
+    const res = await api.get(`${BASE_URL}/auth`);
 
-		dispatch({
-			type: USER_LOADED,
-			payload: res.data,
-		});
-	} catch (err) {
-		dispatch({ type: AUTH_ERROR });
-	}
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({ type: AUTH_ERROR });
+  }
 };
 
 export const register =
-	({ name, email, githubusername, password }) =>
-	async (dispatch) => {
-		const body = { name, email, githubusername, password };
+  ({ name, email, githubusername, password }) =>
+  async (dispatch) => {
+    const body = { name, email, githubusername, password };
 
-		try {
-			const res = await api.post(`${BASE_URL}/api/users`, body);
+    try {
+      const res = await api.post(`${BASE_URL}/users`, body);
 
-			dispatch({
-				type: REGISTER_SUCCESS,
-				payload: res.data,
-			});
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
 
-			dispatch(loadUser());
-		} catch (err) {
-			const errors = err.response.data.errors;
-			if (errors) {
-				errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
-			}
+      dispatch(loadUser());
+    } catch (err) {
+      const errors = err.response.data.errors;
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+      }
 
-			dispatch({ type: REGISTER_FAIL });
-		}
-	};
+      dispatch({ type: REGISTER_FAIL });
+    }
+  };
 
 export const login = (email, password) => async (dispatch) => {
-	const body = { email, password };
+  const body = { email, password };
 
-	try {
-		const res = await api.post(`${BASE_URL}/api/auth`, body);
+  try {
+    const res = await api.post(`${BASE_URL}/auth`, body);
 
-		dispatch({
-			type: LOGIN_SUCCESS,
-			payload: res.data,
-		});
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
 
-		dispatch(loadUser());
-	} catch (err) {
-		const errors = err.response.data.errors;
-		if (errors) {
-			errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
-		}
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
 
-		dispatch({ type: LOGIN_FAIL });
-	}
+    dispatch({ type: LOGIN_FAIL });
+  }
 };
 
 export const logout = () => (dispatch) => {
-	dispatch({ type: LOGOUT });
+  dispatch({ type: LOGOUT });
 };

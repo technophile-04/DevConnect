7915637b17b8e907ac73286ce4cea89d9d9420bc
Shiv Bhonde <shivbhonde04@gmail.com>
2022-05-31 10:53:30 +0530

    update base url

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'http://localhost:8080/api';
-// export const BASE_URL = 'https://dev-connect-sap.herokuapp.com/';
-export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL = 'https://dev-connect-sap.herokuapp.com/api';
+//export const BASE_URL = "http://localhost:8080/api";
